686383db81854ec48ab99f840f7d43840a557a49
Alexa2502 <129477351+Alexa2502@users.noreply.github.com>
2023-03-30 23:14:18 -0500

    Update gc-tig.js

--- comandos/gc-tig.js
+++ comandos/gc-tig.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*▪𝐁𝐨𝐭𝐉𝐞𝐧𝐧𝐱𝐓𝐚𝐭𝐢:* ${pesan}`
+let oi = `*▪𝐌𝐨𝐫𝐠𝐚𝐧𝐁𝐨𝐭:* ${pesan}`
 let teks = `*⺀𝐀𝐂𝐓𝐈𝐕𝐄𝐍𝐒𝐄 𝐏𝐈𝐍𝐆𝐎𝐍𝐄𝐒⺀*\n\n▪ ${oi}\n\n▪ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚 𝐠𝐞𝐧𝐞𝐫𝐚𝐥:*\n`
 for (let mem of participants) {
-teks += `┣✉️ @${mem.id.split('@')[0]}\n`}
-teks += `└𝐉𝐞𝐧𝐧𝐱𝐓𝐚𝐭𝐢`
+teks += `┣👑 @${mem.id.split('@')[0]}\n`}
+teks += `└𝐌𝐨𝐫𝐠𝐚𝐧𝐁𝐨𝐭`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tig <mesaje>','tati <mesaje>']
